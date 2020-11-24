628b66be3ccf6fa3f3fc7e0f7fb26099b3e6ac7a
Andrew Circelli <andrewcircelli@gmail.com>
2020-11-24 08:17:09 -0500

    initial commit MVC v1

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE user_db;
+USE user_db;
+
+CREATE TABLE users
+(
+    id int NOT NULL AUTO_INCREMENT
+    name varchar(255) NOT NULL
+    categories varchar(255) NOT NULL,
+    location varchar(255),
+    PRIMARY KEY (id)
+);
+
+-- QUESTIONS
+-- 1. String vs integer for location
+-- 2. What should or shouldn't be NOT NULL
