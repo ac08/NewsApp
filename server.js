1e7686d97e0deda64be461aacfe9e2dfbc377953
Andrew Circelli <andrewcircelli@gmail.com>
2020-11-24 08:40:18 -0500

    sequelize init

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-var express = require("express");
+const express = require("express");
+const path    = require('path');
+const exphbs  = require("express-handlebars");
 
-var PORT = process.env.PORT || 3000;
 
-var app = express();
+const PORT = process.env.PORT || 8080;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+const app = express();
 
-// Parse request body as JSON
+// Sets up the Express app to establish a static directory to access static files
+app.use(express.static(path.join(__dirname, '/public')));
+// Serve the corresponding node_modules folder if file is not found in public
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+
+// Parse application body as JSON, url
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
+// Sets up template engine and defaultLayout
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
-
+const routes = require("./controllers/users_controller.js");
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () => {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
