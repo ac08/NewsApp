cab34f723968a330a05ae3b101a90e106f8ebaba
Andrew Circelli <andrewcircelli@gmail.com>
2020-11-24 08:19:32 -0500

    added JAWSDB functionality to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,28 @@
-var mysql = require("mysql");
+// Set up MySQL connection.
+const mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "root",
-    database: "user_db"
+    password: "Cleveland08!",
+    database: "users_db"
+  });
+};
+
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
+// Export connection for our ORM to use.
 module.exports = connection;
+
