3335719c694d7ecd9ec1ef3fd309490383d01bfa
Andrew Circelli <andrewcircelli@gmail.com>
2020-11-24 09:03:58 -0500

    refactor server.js

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+
 const express = require("express");
 const path    = require('path');
 const exphbs  = require("express-handlebars");
 
-
+// Sets up the Express App
+// =============================================================
 const PORT = process.env.PORT || 8080;
-
 const app = express();
+
+// Requiring our models for syncing
+const db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Sets up the Express app to establish a static directory to access static files
 app.use(express.static(path.join(__dirname, '/public')));
@@ -15,23 +29,20 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/popper/dist')));
 
-
-// Parse application body as JSON, url
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 // Sets up template engine and defaultLayout
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.set('views', __dirname, '/views');
 
+// Routes
+// =============================================================
+require("./controllers/users_controller.js")(app);
 
-// Import routes and give the server access to them.
-const routes = require("./controllers/users_controller.js");
-app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, () => {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
+
