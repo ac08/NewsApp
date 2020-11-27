f9bd001c6dae0614d866c84544ae8478dc761569
Andrew Circelli <andrewcircelli@gmail.com>
2020-11-26 21:14:39 -0500

    refactor sequelize models to add val and hooks

--- models/article.js
+++ models/article.js
@@ -1,12 +1,26 @@
+// attribute - value pair for Article model
 module.exports = (sequelize, DataTypes) => {
   const Article = sequelize.define('Article', {
     source_nm: DataTypes.STRING,
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    url: DataTypes.STRING,
-    urlToImage: DataTypes.STRING,
-    publishedAt: DataTypes.STRING,
-    content: DataTypes.STRING
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    description: DataTypes.TEXT,
+    url: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: true
+      }
+    },
+    urlToImage: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: true
+      }
+    },
+    publishedAt: DataTypes.STRING, // DOES THIS NEED TO BE DATETIME/DATE TYPE?
+    content: DataTypes.TEXT
   });
   return Article;
 };
--- models/user.js
+++ models/user.js
@@ -1,20 +1,41 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    first_name: {
+    first_nm: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    last_name: {
+    last_nm: {
       type: DataTypes.STRING,
       allowNull: false
     },
+    full_nm: DataTypes.STRING,
     country_cd: {
       type: DataTypes.STRING,
-      defaultValue: 'en',
+      defaultValue: 'us',
       validate: {
-        len: [2]
+        isIn: {
+          args: [['ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'in', 'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za']],
+          msg: 'Error: Field must be valid country code (us, it, jp).'
+        }
+      }
+    }
+  }, {
+    hooks: {
+      beforeCreate: (user) => {
+        user.full_nm = `${user.first_nm} ${user.last_nm}`;
       }
     }
   });
   return User;
 };
+
+// example catch on User.country_cd validation error
+// app.get('/', (req, res) => {
+//   User.create([
+//   first_nm: 'John',
+//   last_nm: 'Doe',
+//   country_cd: 'zz'
+//   ])
+//   .then((user) => { res.json(user) });
+//   .catch((err) => { console.log(err); res.status(404).send(err) });
+// });
