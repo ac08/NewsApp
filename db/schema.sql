86c5ee613a21db95c3ef259964fd176f07aa261b
Andrew Circelli <andrewcircelli@gmail.com>
2020-12-06 21:02:47 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,39 @@
 -- https://dbdiagram.io/d/5fbef5fd3a78976d7b7d72fd
 
-CREATE DATABASE user_db;
-USE user_db;
+CREATE DATABASE news;
+USE news;
 
 CREATE TABLE `users` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255),
   `last_name` varchar(255),
-  `created_at` timestamp COMMENT 'When user created',
+  `full_name` varchar(255),
+  `created_at` timestamp COMMENT 'When order created',
   `country_code` char(2)
 );
 
-CREATE TABLE `countries` (
-  `code` char PRIMARY KEY,
-  `name` varchar(255)
-);
-
 CREATE TABLE `categories` (
-  `user_id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_id` int,
   `category` varchar(255)
 );
 
-ALTER TABLE `countries` ADD FOREIGN KEY (`code`) REFERENCES `users` (`country_code`);
+CREATE TABLE `articles` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `source_nm` varchar(255),
+  `title` text,
+  `description` text,
+  `url` varchar(255),
+  `urlToImage` varchar(255),
+  `content` text,
+  `publishedAt` datetime
+);
+
+CREATE TABLE `UserCategories` (
+  `categoryId` int,
+  `userId` int
+);
 
 ALTER TABLE `categories` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
-
-
+ALTER TABLE `articles` ADD FOREIGN KEY (`id`) REFERENCES `categories` (`id`);
